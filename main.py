77344af89ea1ec6beb3e5036f670fdae9f522e2d
Tom Selfin <selfint@gmail.com>
2020-11-18 08:47:51 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+from string_sum import sum_as_string
+
+if __name__ == "__main__":
+    print(sum_as_string(1, 2))
---
+++ string_sum.pyi
@@ -0,0 +1,4 @@
+""" A Python module implemented in Rust. """
+
+def sum_as_string(a: int, b: int) -> str:
+    """ Sum two numbers as a string """
